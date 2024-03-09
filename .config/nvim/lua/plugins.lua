25199909e3dc81768ea5722487db12f6b2db6270
0xDkXy <0xdkxy.pollardlee@gmail.com>
2024-03-10 01:42:49 +0800

    update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,20 +1,44 @@
 
+vim.g.mapleader = " "
 
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
+require("lazy").setup({
+    -- nvim-treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+    },
+    "wakatime/vim-wakatime",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "neovim/nvim-lspconfig",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "nvim-lua/plenary.nvim",
 
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/vim-vsnip",
+    "hrsh7th/nvim-cmp",
 
-    use {'wfxr/minimap.vim', run = 'paru -S code-minimap-git'}
+    {
+        "akinsho/bufferline.nvim",
+        version = "*",
+        dependencies = "nvim-tree/nvim-web-devicons"
+    },
 
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function() 
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true})
-            ts_update()
-        end,
+    "chentoast/marks.nvim",
+    "folke/flash.nvim",
+
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        }
     }
-    use 'nvim-treesitter/nvim-treesitter-textobjects'
-    use 'neovim/nvim-lspconfig'
-    use 'williamboman/mason.nvim'
-end)
+})
 
--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -1,6 +1,6 @@
 -- config for treesitter
 require 'nvim-treesitter.configs'.setup{
-    ensure_installed = {"c", "cpp", "python", "lua", "vim", "help"},
+    ensure_installed = {"c", "cpp", "python", "lua", "vim"},
     sync_install = false,
     auto_install = true,
     highlight = {
@@ -15,12 +15,92 @@ require("mason").setup({
         install_args = {"--proxy", "http://127.0.0.1:7893"},
     },
 })
+require("mason-lspconfig").setup()
 
 
 require 'lspconfig'.pyright.setup{}
 require 'lspconfig'.clangd.setup{}
+require 'lspconfig'.verible.setup{}
 
 -- config for treesitter
 require("nvim-treesitter.install").command_extra_args = {
     curl = {"--proxy", "http://127.0.0.1:7893"},
 }
+
+
+-- nvim-cmp
+local cmp = require("cmp")
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+        end,
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' },
+    }, {
+        { name = 'buffer' },
+    })
+
+})
+
+cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        {
+            name = 'cmdline',
+            option = {
+                ignore_cmds = { 'Man', '!'}
+            }
+        }
+    })
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require('lspconfig')['clangd'].setup {
+    capabilities = capabilities
+}
+require('lspconfig')['verible'].setup {
+    capabilities = capabilities
+}
+
+-- buffer line config
+vim.opt.termguicolors = true
+require("bufferline").setup({
+    options = {
+        mode = "buffers"
+    }
+})
+
+-- marks config
+require("marks").setup()
+
+-- flash.nvim 
+require("flash").setup()
+
+-- config for minimap
+vim.g.minimap_width = 10
+vim.g.minimap_auto_start = 1
+vim.g.minimap_auto_start_win_enter = 1
