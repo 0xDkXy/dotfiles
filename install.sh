c4476220106c8e6f2ae75e37cd1b30a72d9ed882
0xDkXy <0xdkxy.pollardlee@gmail.com>
2024-05-16 14:02:58 +0000

    update proxychains configuration

--- install.sh
+++ install.sh
@@ -2,3 +2,11 @@
 
 
 cp bashrc ~/.bashrc
+cp vimrc ~/.vimrc
+cp tmux.conf ~/.tmux.conf 
+cp -r config/* ~/.config/
+
+if [[ ! -d "~/.proxychains" ]]; then
+    mkdir -p ~/.proxychains
+fi
+cp proxychains.conf ~/.proxychains/
