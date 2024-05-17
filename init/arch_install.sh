a9daf6590d90920571f08ee05924033d754ed35e
0xDkXy <0xdkxy.pollardlee@gmail.com>
2024-05-17 15:41:36 +0000

    update install packages

--- init/arch_install.sh
+++ init/arch_install.sh
@@ -2,5 +2,8 @@
 
 pacman -S neovim hyprland tmux git sx lxsession \
 	firefox alacritty grim vim wget curl aria2 \
-	alsa-utils
+	alsa-utils sof-firmware proxychains base-devel \
+    neomutt pulseaudio pulseaudio-alsa pulsemixer \
+    chrony networkmanager tealdeer rustup clang terminus-font \
+    --needed
 	
